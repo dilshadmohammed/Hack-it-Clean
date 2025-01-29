7e87e149d98e32e1d67acf5f818200d5d665288e
NAYANJITH <nayanjithajith@gmail.com>
2025-01-29 22:40:01 +0530

    Burger nav button

--- src/components/pages/Homepage/Homepage.js
+++ src/components/pages/Homepage/Homepage.js
@@ -10,18 +10,22 @@ export default function Homepage() {
     }
     return(
         <div class="Homepage">
+            <div className="bubbles1">
                 <img className="bubble1" src="/img/bubble-1.png"/>
+                <img className="bubble5" src="/img/bubble-1.png"/>
+                <img className="bubble10" src="/img/bubble-3.png"/>
+                <img className="bubble9" src="/img/bubble-2.png"/>
+                <img className="bubble8" src="/img/bubble-1.png"/>
+            </div>
+            <div className="bubbles2">
                 <img className="bubble2" src="/img/bubble-2.png"/>
                 <img className="bubble3" src="/img/bubble-3.png"/>
                 <img className="bubble4" src="/img/bubble-3.png"/>
-                <img className="bubble5" src="/img/bubble-1.png"/>
                 <img className="bubble6" src="/img/bubble-1.png"/>
                 <img className="bubble7" src="/img/bubble-1.png"/>
-                <img className="bubble8" src="/img/bubble-1.png"/>
-                <img className="bubble9" src="/img/bubble-2.png"/>
-                <img className="bubble10" src="/img/bubble-3.png"/>
                 <img className="bubble11" src="/img/bubble-3.png"/>
-            <div>
+            </div>
+            <div className="details">
                 <div className="line"></div>
                 <span className="title-1">WELCOME</span>
                 <div className="title-2-3">
